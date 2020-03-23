9b3b71ba9a8bc0a07df1be4ee4063d9e16fdc267
Nathália Pissuti <nathaliapissuti@gmail.com>
2020-03-22 22:36:42 -0300

    Tratando css com webpack

--- projeto/client/webpack.config.js
+++ projeto/client/webpack.config.js
@@ -1,10 +1,23 @@
 const path = require('path');
 const babiliPlugin = require('babili-webpack-plugin');
+const extractTextPlugin = require('extract-text-webpack-plugin');
+const optimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 
 const plugins = [];
 
+plugins.push(new extractTextPlugin('styles.css'));
+
 if(process.env.NODE_ENV === 'production') {
     plugins.push(new babiliPlugin());
+    plugins.push(new optimizeCssAssetsPlugin({
+        cssProcessor: require('cssnano'),
+        cssProcessorOptions: {
+            discardComments: {
+                removeAll: true
+            }
+        },
+        canPrint: true
+    }));
 }
 
 module.exports = {
@@ -22,7 +35,30 @@ module.exports = {
                 use: {
                     loader: 'babel-loader'
                 }
-            }
+            },
+            { 
+                test: /\.css$/, 
+                use: extractTextPlugin.extract({
+                    fallback: 'style-loader',
+                    use: 'css-loader'
+                })
+            },
+            { 
+                test: /\.(woff|woff2)(\?v=\d+\.\d+\.\d+)?$/, 
+                loader: 'url-loader?limit=10000&mimetype=application/font-woff' 
+            },
+            { 
+                test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, 
+                loader: 'url-loader?limit=10000&mimetype=application/octet-stream'
+            },
+            { 
+                test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, 
+                loader: 'file-loader' 
+            },
+            { 
+                test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, 
+                loader: 'url-loader?limit=10000&mimetype=image/svg+xml' 
+            }            
         ]
     },
     plugins
